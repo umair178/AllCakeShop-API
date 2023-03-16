ebfa4d28769b2341e66425c7a23f9567db51802f
unknown <umairshabbir178@gmail.com>
2023-03-16 00:48:13 -0700

    add params routes

--- Routes/cakes.js
+++ Routes/cakes.js
@@ -1,10 +1,11 @@
 const express = require('express');
 const router = express.Router();
 const app = express();
-const fs = require('fs')
 const url  = require('url')
 const { v4: uuidv4 } = require('uuid');
 const categoriesController = require('../controllers/categoriescontroller');
+const knex = require('knex')(require('../knexfile'));
+
 
 app.use('/', (req, _res, next) => {
     console.log(req.url);
@@ -15,8 +16,14 @@ router
 .route('/cakes')
 .get(categoriesController.index);
 
-// router.get('/cakes', (req, res) => {
-//     console.log(req.url)
-//         res.json('hello')
-//     });
+
+router.get('/:cakeId', (req, res) => {
+    knex
+        .select('*')
+        .from('cakes')
+        .where('cake_id', req.params.cakeId)
+        .then((cake) => {
+            res.json(cake)
+        })
+});
 module.exports = router
